ab3d4246bebde15ba9e4c6eae67713571ca8b4a6
Varun Barad <varun@varunbarad.com>
2023-07-09 16:30:43 +0530

    Add argument parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let program_arguments = Command::new("grit")
+        .version("0.1.0")
+        .author("Varun Barad <contact@varunbarad.com>")
+        .about("A tiny model of git built using Rust to learn git")
+        .args_override_self(true)
+        .subcommand_required(true)
+        .subcommand(Command::new("greet").about("Say hi to the world"))
+        .get_matches();
+
+    match program_arguments.subcommand() {
+        Some(("greet", _arguments)) => greet(),
+        _ => eprintln!("No known pattern found"),
+    }
+}
+
+fn greet() {
+    println!("Hi, world!");
 }
